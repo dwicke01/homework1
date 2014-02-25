db1c6eb876878a1d70d61429aa616b85835d3bbf
dwicke01 <dwicke01@students.poly.edu>
2014-02-25 18:22:18 -0500

    Initial Commit

---
+++ BlackJack/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlackJack
+//
+//  Created by Daniel Wickes on 2/25/14.
+//  Copyright (c) 2014 Daniel Wickes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlackJack/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlackJack
+//
+//  Created by Daniel Wickes on 2/25/14.
+//  Copyright (c) 2014 Daniel Wickes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
